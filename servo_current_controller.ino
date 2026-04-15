037922bb310ded657260a75fe231abf57851af78
Arduino OTA <arduino-ota@local>
2026-04-15 11:24:17 -0400

    OTA v5: auto-build 2026-04-15 11:24

--- servo_current_controller.ino
+++ servo_current_controller.ino
@@ -111,8 +111,8 @@ const int WEB_SERVER_PORT = 80;
 // -- OTA firmware update ------------------------------------------------------
 // Increment FW_VERSION, then run push_firmware.ps1 to publish a new release.
 // The Arduino polls GitHub every OTA_CHECK_INTERVAL_MS milliseconds.
-#define FW_VERSION            4
-#define FW_VERSION_STR        "4"
+#define FW_VERSION            5
+#define FW_VERSION_STR        "5"
 #define OTA_CHECK_INTERVAL_MS (5UL * 60UL * 1000UL)  // 5 minutes
 
 // ============================================================
